4ac3c32f53e992a9f516e33cf7228a310bb88fd5
morishin <sntr92@gmail.com>
2024-11-01 11:11:35 +0900

    Backoff

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
 import { tweet } from "./tweeter/mod.ts";
 
-Deno.cron("Post a tweet", "0 * * * *", tweet);
+Deno.cron("Post a tweet", "0 * * * *", tweet, {
+  backoffSchedule: [5 * 1000, 3 * 60 * 1000, 17 * 60 * 1000],
+});
