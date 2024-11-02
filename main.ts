9ef1b636c207d29d2216af5074a7ee7dfaa0853a
morishin <sntr92@gmail.com>
2024-11-02 18:14:08 +0900

    Fix cron

--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
 import { reply, tweet } from "./tweeter/mod.ts";
 
-// Rate limit: https://developer.x.com/en/portal/products
+// Due to the rate limit for `POST /2/tweets`
+// => 17 requests / 24 hours & 500 posts / month
+// See: https://developer.x.com/en/portal/products
 Deno.cron(
-  "Post a tweet and reply to mentions",
-  // Due to the rate limit for GET /2/users/:id/mentions
-  // => 1 requests / 15 mins
-  { minute: { every: 20 } },
-  async () => {
-    await reply();
-    const now = new Date();
-    const minute = now.getMinutes();
-    const hour = now.getHours();
-    // Due to the rate limit for POST /2/tweets
-    // => 17 requests / 24 hours & 500 posts / month
-    if (7 <= hour && Math.floor(minute / 20) < 1) {
-      await tweet();
-    } else {
-      console.info("Skip posting a tweet");
-    }
-  },
+  "Post a tweet",
+  "0 8-23 * * *",
   {
     backoffSchedule: [],
-  }
+  },
+  tweet
+);
+
+// Due to the rate limit for `GET /2/users/:id/mentions`
+// => 1 requests / 15 mins
+// See: https://developer.x.com/en/portal/products
+Deno.cron(
+  "Reply to mentions if exist",
+  { minute: { every: 20 } },
+  {
+    backoffSchedule: [],
+  },
+  reply
 );
